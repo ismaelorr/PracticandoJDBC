12249eae8feff1fc404d4e906013cbe20958635d
ismaelorr <xxismaelor03xx@gmail.com>
2022-04-20 13:53:01 +0200

    Commit 1

---
+++ src/main/java/edu/fpdual/ejemplo/jdbc/conector/MySQLConector.java
@@ -0,0 +1,70 @@
+package edu.fpdual.ejemplo.jdbc.conector;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ * Clase que crea la conexion con la base de datos
+ * @author Ismael Orellana Bello
+ *
+ */
+public class MySQLConector {
+
+
+    @Getter
+    @Setter
+    Properties prop = new Properties();
+
+    public MySQLConector(){
+        try{
+            prop.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Creates the connection object for a MySQL DDBB
+     * @return a {@link Connection}
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public Connection getMySQLConnection() throws ClassNotFoundException, SQLException {
+        try {
+
+            //Indicates which driver is going to be used.
+            Class.forName(prop.getProperty(MySQLConstants.DRIVER));
+            //Creates the connection based on the obtained URL.
+            return  DriverManager.getConnection(getUrl());
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+
+    private String getUrl(){
+        return new StringBuilder().append(prop.getProperty(MySQLConstants.URL_PREFIX))
+                .append(prop.getProperty(MySQLConstants.URL_HOST)).append(":")
+                .append(prop.getProperty(MySQLConstants.URL_PORT)).append("/")
+                .append(prop.getProperty(MySQLConstants.URL_SCHEMA)).append("?user=")
+                .append(prop.getProperty(MySQLConstants.USER)).append("&password=")
+                .append(prop.getProperty(MySQLConstants.PASSWD)).append("&useSSL=")
+                .append(prop.getProperty(MySQLConstants.URL_SSL)).append(("&useJDBCCompliantTimezoneShift="))
+                .append(prop.getProperty(MySQLConstants.USE_JDBC_COMPLIANT_TIMEZONE_SHIFT)).append(("&useLegacyDatetimeCode="))
+                .append(prop.getProperty(MySQLConstants.USE_LEGACY_DATE_TIME_CODE)).append(("&serverTimezone="))
+                .append(prop.getProperty(MySQLConstants.SERVER_TIMEZONE)).toString();
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        MySQLConector connector = new MySQLConector();
+        Connection connection = connector.getMySQLConnection();
+        System.out.println(connection.getCatalog());
+    }
+
+}
+
+
---
+++ src/main/java/edu/fpdual/ejemplo/jdbc/conector/MySQLConstants.java
@@ -0,0 +1,24 @@
+package edu.fpdual.ejemplo.jdbc.conector;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+public class MySQLConstants {
+
+    public static final String PASSWD = "jdbc.mysql.passwd";
+    public static final String USER = "jdbc.mysql.user";
+    public static final String DRIVER = "jdbc.mysql.driver";
+    public static final String URL_PREFIX = "jdbc.mysql.url.prefix";
+    public static final String URL_HOST = "jdbc.mysql.url.host";
+    public static final String URL_PORT = "jdbc.mysql.url.port";
+    public static final String URL_SCHEMA = "jdbc.mysql.url.schema";
+    public static final String URL_SSL = "jdbc.mysql.url.ssl";
+    public static final String USE_JDBC_COMPLIANT_TIMEZONE_SHIFT = "jdbc.mysql.url.useJDBCCompliantTimezoneShift";
+    public static final String USE_LEGACY_DATE_TIME_CODE = "jdbc.mysql.url.useLegacyDatetimeCode";
+    public static final String SERVER_TIMEZONE = "jdbc.mysql.url.serverTimezone";
+
+}
